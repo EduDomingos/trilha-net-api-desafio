728dc0c4583fddc15d9ca3cedd51150e33000b75
edudomingos <edudomingos@gmail.com>
2024-05-26 10:02:24 -0300

    Solucao Final

--- Controllers/TarefaController.cs
+++ Controllers/TarefaController.cs
@@ -8,17 +8,17 @@ namespace TrilhaApiDesafio.Controllers
     [Route("[controller]")]
     public class TarefaController : ControllerBase
     {
-        private readonly OrganizadorContext _context;
+        private readonly OrganizadorContext context;
 
         public TarefaController(OrganizadorContext context)
         {
-            _context = context;
+            this.context = context;
         }
 
         [HttpGet("{id}")]
         public IActionResult ObterPorId(int id)
         {
-            var tarefa = _context.Tarefas.Find(id);
+            var tarefa = this.context.Tarefas.Find(id);
 
             if (tarefa == null)
                 return NotFound();
@@ -29,28 +29,28 @@ namespace TrilhaApiDesafio.Controllers
         [HttpGet("ObterTodos")]
         public IActionResult ObterTodos()
         {
-            var tarefa = _context.Tarefas.ToList();
+            var tarefa = this.context.Tarefas.ToList();
             return Ok(tarefa);
         }
 
         [HttpGet("ObterPorTitulo")]
         public IActionResult ObterPorTitulo(string titulo)
         {
-            var tarefa = _context.Tarefas.Where(x => x.Titulo.ToUpper().Contains(titulo.ToUpper())).ToList();
+            var tarefa = this.context.Tarefas.Where(x => x.Titulo.ToUpper().Contains(titulo.ToUpper())).ToList();
             return Ok(tarefa);
         }
 
         [HttpGet("ObterPorData")]
         public IActionResult ObterPorData(DateTime data)
         {
-            var tarefa = _context.Tarefas.Where(x => x.Data.Date == data.Date);
+            var tarefa = this.context.Tarefas.Where(x => x.Data.Date == data.Date);
             return Ok(tarefa);
         }
 
         [HttpGet("ObterPorStatus")]
         public IActionResult ObterPorStatus(EnumStatusTarefa status)
         {
-            var tarefa = _context.Tarefas.Where(x => x.Status == status);
+            var tarefa = this.context.Tarefas.Where(x => x.Status == status);
             return Ok(tarefa);
         }
 
@@ -60,15 +60,15 @@ namespace TrilhaApiDesafio.Controllers
             if (tarefa.Data == DateTime.MinValue)
                 return BadRequest(new { Erro = "A data da tarefa não pode ser vazia" });
 
-            _context.Tarefas.Add(tarefa);
-            _context.SaveChanges();
+            this.context.Tarefas.Add(tarefa);
+            this.context.SaveChanges();
             return CreatedAtAction(nameof(ObterPorId), new { id = tarefa.Id }, tarefa);
         }
 
         [HttpPut("{id}")]
         public IActionResult Atualizar(int id, Tarefa tarefa)
         {
-            var tarefaBanco = _context.Tarefas.Find(id);
+            var tarefaBanco = this.context.Tarefas.Find(id);
 
             if (tarefaBanco == null)
                 return NotFound();
@@ -81,8 +81,8 @@ namespace TrilhaApiDesafio.Controllers
             tarefaBanco.Data = tarefa.Data;
             tarefaBanco.Status = tarefa.Status;
 
-            _context.Tarefas.Update(tarefaBanco);
-            _context.SaveChanges();
+            this.context.Tarefas.Update(tarefaBanco);
+            this.context.SaveChanges();
 
             return Ok();
         }
@@ -90,13 +90,13 @@ namespace TrilhaApiDesafio.Controllers
         [HttpDelete("{id}")]
         public IActionResult Deletar(int id)
         {
-            var tarefaBanco = _context.Tarefas.Find(id);
+            var tarefaBanco = this.context.Tarefas.Find(id);
 
             if (tarefaBanco == null)
                 return NotFound();
 
-            _context.Tarefas.Remove(tarefaBanco);
-            _context.SaveChanges();
+            this.context.Tarefas.Remove(tarefaBanco);
+            this.context.SaveChanges();
 
             return NoContent();
         }
